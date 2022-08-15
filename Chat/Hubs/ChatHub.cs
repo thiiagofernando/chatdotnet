fc0fa35e3fa45104dbf0c5510a18590671abbd63
Thiago Fernando <thiago.fernando@msn.com>
2022-08-15 19:32:10 -0400

    Criado hub

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+
+using Chat.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Chat.Repository;
+using Newtonsoft.Json;
+
+
+namespace Chat.Hubs
+{
+   public class ChatHub : Hub
+    {
+        private readonly static ConnectionsRepository _connections = new ConnectionsRepository();
+
+        public override Task OnConnectedAsync()
+        {
+            var user = JsonConvert.DeserializeObject<Usuario>(Context.GetHttpContext().Request.Query["user"]);
+            _connections.Adicionar(Context.ConnectionId, user);
+
+            Clients.All.SendAsync("chat", _connections.ObterTodosUsuarios(), user);
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(Mensagem chat)
+        {
+            await Clients.Client(_connections.ObterUsuarioPorId(chat.Destino)).SendAsync("Receive", chat.Remetente, chat.Texto);
+        }
+    }
+}
